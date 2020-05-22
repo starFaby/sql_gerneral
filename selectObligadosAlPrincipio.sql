39cebb8906144a75828f809b440cc8151d2a2fe7
starFaby <star._faby@hotmail.com>
2020-05-22 13:01:00 -0500

    selesct obligatorios

---
+++ selectObligadosAlPrincipio.sql
@@ -0,0 +1,5 @@
+INSERT INTO `eurekakits`.`tipopago` (`nombre`, `estado`, `created_at`) VALUES ('Paypal', '1', '2020-05-22');
+INSERT INTO `eurekakits`.`tipopago` (`nombre`, `estado`, `created_at`) VALUES ('Transferencia Bancaria', '1', '2020-05-22');
+INSERT INTO `eurekakits`.`tipopago` (`nombre`, `estado`, `created_at`) VALUES ('Efectivo', '1', '2020-05-22');
+INSERT INTO `categoria` (`nombre`, `image`, `estado`, `created_at`) VALUES ('herramientas mecanicas', '/uploads/1583430725929.jpg', 1, CURRENT_TIMESTAMP);
+INSERT INTO `eurekakits`.`dto` (`dto`, `estado`, `created_at`) VALUES ('5', '1', '2020-05-23');
